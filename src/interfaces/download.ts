4e1a01a0f526aedce93cc6cffa0e98a600262243
CaoMeiYouRen <996881204@qq.com>
2024-12-01 15:50:49 +0800

    fix(download): 修正 aria2 下载器名称 - 将 aria2 下载器名称从 'aria2' 修正为 'aria2c'

--- src/interfaces/download.ts
+++ src/interfaces/download.ts
@@ -2,7 +2,7 @@
 export const EngineEnum = {
     AUTO: 'auto', // 自动选择下载器，默认使用 you-get
     YOU_GET: 'you-get', // 用于下载视频
-    ARIA2: 'aria2', // 用于下载直链视频
+    ARIA2: 'aria2c', // 用于下载直链视频
     YUTTO: 'yutto', // 用于 bilibili
     YOUTUBE_DL: 'youtube-dl', // 用于 youtube
     YT_DLP: 'yt-dlp', // 用于 youtube
